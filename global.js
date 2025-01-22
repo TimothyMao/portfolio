e725c224d1dfce1be4689e2928860c6058f56316
TimothyMao <timao@ucsd.edu>
2025-01-22 15:38:57 -0800

    added js file and changed nav links

---
+++ global.js
@@ -0,0 +1,39 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+const navLinks = $$("nav a")
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+// currentLink?.classList.add('current');
+
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'resume/', title: "Resume"},
+    { url: 'https://github.com/TimothyMao', title: "Profile"}
+    // add the rest of your pages here
+  ];
+
+  
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+
+
+  
