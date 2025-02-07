317e86c053bd5330c4c4da83466cb1a457358dbe
TimothyMao <timao@ucsd.edu>
2025-02-07 13:36:50 -0800

    added more colors

--- projects/projects.js
+++ projects/projects.js
@@ -16,29 +16,18 @@ let arc = arcGenerator({
   });
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-let data = [1, 2];
-let total = 0;
-
-for (let d of data) {
-  total += d;
-}
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
-
-let colors = ['gold', 'purple'];
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 arcs.forEach((arc, idx) => {
     d3.select('svg')
       .append('path')
       .attr('d', arc)
-      .attr('fill', colors[idx]);
+      .attr('fill', colors(idx));
 })
   
 
