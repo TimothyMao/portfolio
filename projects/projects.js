60792172ef94d7cb4212c533c72dffda8cf50247
TimothyMao <timao@ucsd.edu>
2025-02-07 14:06:54 -0800

    added search bar

--- projects/projects.js
+++ projects/projects.js
@@ -15,38 +15,53 @@ let arc = arcGenerator({
     endAngle: 2 * Math.PI,
   });
 
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-let rolledData = d3.rollups(
+let query = '';
+
+let searchInput = document.querySelector('.searchBar');
+
+searchInput.addEventListener('change', (event) => {
+  // update query value
+  query = event.target.value;
+  // TODO: filter the projects
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+  let rolledData = d3.rollups(
     projects,
     (v) => v.length,
     (d) => d.year,
   );  
 
-let data = rolledData.map(([year, count]) => {
-    return { value: count, label: year };
-  });
+    let data = rolledData.map(([year, count]) => {
+        return { value: count, label: year };
+    });
 
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
+    let sliceGenerator = d3.pie().value((d) => d.value);
+    let arcData = sliceGenerator(data);
+    let arcs = arcData.map((d) => arcGenerator(d));
 
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
+    let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-arcs.forEach((arc, idx) => {
-    d3.select('svg')
-      .append('path')
-      .attr('d', arc)
-      .attr('fill', colors(idx));
-})
+    arcs.forEach((arc, idx) => {
+        d3.select('svg')
+        .append('path')
+        .attr('d', arc)
+        .attr('fill', colors(idx));
+    })
 
-let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-    legend.append('li')
-          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-})
+    let legend = d3.select('.legend');
+    data.forEach((d, idx) => {
+        legend.append('li')
+            .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+            .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+    })
 
 
+  // TODO: render updated projects
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+});
 
 
   
