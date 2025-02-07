a25b05fdd18743d2a6b5e3597644ddc3d8e55292
TimothyMao <timao@ucsd.edu>
2025-02-07 13:20:57 -0800

    added year under project descriptions

--- global.js
+++ global.js
@@ -128,7 +128,10 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
+      <div class="project-text">
+          <p>${project.description}</p>
+          <p class="project-year">${project.year}</p>
+      </div>
     `;
     containerElement.appendChild(article);
 });
